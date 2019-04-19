98437fd1779e93fcdc6d12f7c12e7db51d8d2bbf
David <dc82927n@gmail.com>
2019-04-18 23:40:33 -0400

    041819

--- slides/js/script.js
+++ slides/js/script.js
@@ -1,11 +1,22 @@
 
 
 $(document).ready(function() {
+
     $('#slides').superslides({
         animation: 'fade',
         play: 5000, 
         pagination: false
     });
+
+    var typed = new Typed(".typed", {
+        strings: [" ", "Web Developer", "Food Connoisseur", "Front-End Developer", "Full Stack Developer"],
+        typeSpeed: 120, 
+        loop: true, 
+        startDelay: 20, 
+        showCursor: false
+    }); 
+
+
 }); 
 
 
