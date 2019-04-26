819e78e9a8543e06747c24a908db75002c02f72c
David <dc82927n@gmail.com>
2019-04-26 03:05:00 -0400

    completed pie chart

--- slides/js/script.js
+++ slides/js/script.js
@@ -16,8 +16,49 @@ $(document).ready(function() {
         showCursor: false
     }); 
 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            }, 
+            938: {
+                item: 4
+            }
+        }
+    });
+    
 
-}); 
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    
+    $(window).scroll(function(){
+        if(window.pageYOffset  > skillsTopOffset - $(window).height() + 300) {
+            console.log(skillsTopOffset)
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: "#c0392b",
+                trackColor: "#ffffff",
+                scalecColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function (from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+    });
+
+
+});
+
+
 
 
 
